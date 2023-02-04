8cfb1240ee0fe7f2dafb0ee94505d07d0c1ed31d
Michal Rutz <michal.rutz@gmail.com>
2023-02-04 23:34:25 +0100

    gallery 2

---
+++ projects.js
@@ -0,0 +1,22 @@
+const tvAdvisor = {
+    title: "TV Show Advisor",
+    tools: "CSS, JavaScript, React",
+    desc: "A search machine for tv shows with a slick minimalistic design. After submiting a correct request application generates a detailed page of the show and a list of recommandations.<br>A single-page application that dynamically generates pages based on third-party API with the use of modern function components and react hooks. This project display my ability to deal with asychronous requests and databases.",
+    img: "./pics/tv_small.jpg"
+};
+const memoGame = {
+    title: "the Memory Game",
+    tools: "CSS, JavaScript, React",
+    desc: "The cards are placed face down. In rows and columns. A player turns over two cards, one at a time. The object of the game is to collect all pairs as soon as possible <br>The application was created with help of react framework and demonstrates an advanced use of react hooks and JavaScript.",
+    img: "./pics/memo_small.png",
+    link:"https://memo-game-six.vercel.app/"
+};
+const SimonGame = {
+    title: "the Memory Game",
+    tools: "CSS, JavaScript, React",
+    desc: "The Simon is a classic memory game. The application creates a series of tones and lights and requires a player to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit runs out, the game is over.",
+    img: "./pics/tv_small.jpg"
+};
+const projects = [tvAdvisor, memoGame, SimonGame];
+module.exports = projects;
+
