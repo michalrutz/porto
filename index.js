7689f517a4b018d70f627957824cd4a9e736e5f7
Michal Rutz <michal.rutz@gmail.com>
2023-02-04 20:10:49 +0100

    js for-loop -> gallery

--- index.js
+++ index.js
@@ -20,3 +20,52 @@ window.smoothScroll = function(target) {
     // start scrolling
     scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
 }
+
+const tvAdvisor = {
+    title: "TV Show Advisor",
+    tools: "CSS, JavaScript, React",
+    desc: "A search machine for tv shows with a slick minimalistic design. After submiting a correct request application generates a detailed page of the show and a list of recommandations.<br>A single-page application that dynamically generates pages based on third-party <b>API</b> with the use of modern <b>function components</b> and react hooks. This project display my ability to deal with asychronous requests and databases.",
+    img: "./pics/tv_small.jpg"
+}
+const memoGame = {
+    title: "the Memory Game",
+    tools: "CSS, JavaScript, React",
+    desc: "The cards are placed face down. In rows and columns. A player turns over two cards, one at a time. The object of the game is to collect all pairs as soon as possible.",
+    img: "./pics/memo_small.png"
+}
+const SimonGame = {
+    title: "the Memory Game",
+    tools: "CSS, JavaScript, React",
+    desc: "The Simon is a classic memory game. The application creates a series of tones and lights and requires a player to repeat the sequence. If the user succeeds, the series becomes progressively longer and more complex. Once the user fails or the time limit runs out, the game is over.",
+    img: "./pics/tv_small.jpg"
+}
+
+const projects = [tvAdvisor, memoGame, SimonGame]
+
+function gallery(array) {
+    let gallery = [...array];
+    let list = [];
+    for (let i = 0; i < gallery.length; i++) {
+        const project = gallery[i];
+        list.push(
+            `
+            <div id='${(i)+"project"}' class="singleProject ${i%2===0 ? "odd" : ""}">
+                <div class="singleProject_frame">
+                    <h3 class="title">${project.title}</h3>
+                    <p class="shortDiscription">languages: ${project.tools}</p>
+                    <p>${project.desc}</p>
+                </div>
+                <img src="${project.img}" class="view">
+                <div class="projectGap" >
+                    <button style="display:${i===gallery.length-1 ? "none" : "block"}" class="next btn1" type="button" onclick="smoothScroll(document.getElementById('${(i+1)+"project"}'))">next ↓</button>
+                    <button style="display:${i!==gallery.length-1 ? "none" : "block"}" class="next btn1" type="button" onclick="smoothScroll(document.getElementById('${(0)+"project"}'))">back ↑</button>
+                    </div> 
+            </div>
+            `
+        )
+    }
+    list.unshift(`<h2 id="0project">PROJECTS</h2>`)
+    console.log(list);
+    return list
+}
+document.getElementById("Projects").innerHTML=gallery(projects).join("");
